8d07f9a8bdaaa461c683440bc812bfb116aa4042
jrclarete <jhonrommel.clarete@gmail.com>
2021-10-22 22:05:46 +0800

    Create login and logout functionality

--- src/Security/Akasia.Security/Controllers/ClientsController.cs
+++ src/Security/Akasia.Security/Controllers/ClientsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace Akasia.Security.Controllers
 {
+    [Authorize]
     public class ClientsController : Controller
     {
         private readonly ILogger<IdentityResourcesController> _logger;
